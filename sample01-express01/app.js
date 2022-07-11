d4846ca720150b178bf787817f83fd6699039cf8
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-07-11 17:12:20 +0900

    add sample01

---
+++ sample01-express01/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  const { params, query } = req;
+  console.table({ params, query });
+  res.send(params);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const courses = require("./courses");
+  const targetCourse = courses.filter((c) => c.id === Number(req.params.id));
+
+  if (!targetCourse[0]) return res.sendStatus(404);
+  return res.send(targetCourse[0]);
+});
+
+app.post("/api/courses", (req, res) => {
+  const { error } = validateCourses(req.body);
+  if (error) {
+    res.status(400).send(error.message);
+    return;
+  }
+
+  const courses = require("./courses");
+  const { name } = req.body;
+
+  const newCourse = {
+    id: courses.length + 1,
+    name,
+  };
+
+  courses.push(newCourse);
+  res.send(newCourse);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const courses = require("./courses");
+  let { id } = req.params;
+  id = Number(id);
+
+  let deleteCourse = null;
+  for (let i = 0; i < courses.length; i++) {
+    if (courses[i].id === id) deleteCourse = courses[i];
+    if (deleteCourse) {
+      courses[i] = courses[i + 1];
+    }
+  }
+
+  if (!deleteCourse) return res.sendStatus(404);
+
+  courses.pop();
+  res.send(deleteCourse);
+});
+
+const Joi = require("joi");
+function validateCourses(courses) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(courses);
+}
+
+app.listen(3000, () => {
+  console.log("PORT:", process.env.PORT || 3000);
+});
---
+++ sample01-express01/courses.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
