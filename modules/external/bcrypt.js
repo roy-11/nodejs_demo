c37fe24222e424728d69587a33dd628836e3fa3b
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-08-17 20:36:49 +0900

    add external directory

--- modules/external/bcrypt.js
+++ modules/external/bcrypt.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
 
 const salt = bcrypt.genSaltSync(10);
-console.log(salt);
+const hashed = bcrypt.hashSync("passward", salt);
+console.log("salt:", salt);
+console.log("hashed:", hashed);
